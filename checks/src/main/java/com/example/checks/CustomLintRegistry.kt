b659defb02b1d5a1df867f03e113653837bf4317
armen <mnmagevorgyanweb@gmail.com>
2019-12-05 10:07:05 +0400

    fixed kt warning

--- checks/src/main/java/com/example/checks/CustomLintRegistry.kt
+++ checks/src/main/java/com/example/checks/CustomLintRegistry.kt
@@ -3,11 +3,10 @@ package com.example.checks
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
 import com.sun.istack.NotNull
-import java.util.*
 
 class CustomLintRegistry : IssueRegistry() {
 
     @get:NotNull
-    override val issues: List<Issue> get() = Arrays.asList(TextAppearanceIssue.ISSUE)
+    override val issues: List<Issue> get() = listOf(TextAppearanceIssue.ISSUE)
 
 }
